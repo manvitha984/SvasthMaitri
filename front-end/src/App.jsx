c62d7fdd932cd1e07f70a47b5670429f213b5bd6
Chaitanya-45 <cgatreddi@gmail.com>
2024-06-08 18:18:41 +0530

    added body page bfr login

---
+++ front-end/src/App.jsx
@@ -0,0 +1,18 @@
+import BfrBody from './components/BfrBody';
+import BfrNavbar from './components/BfrNavbar'
+import Footer from './components/Footer'
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+       <BfrNavbar />
+       <BfrBody />
+       <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App;
