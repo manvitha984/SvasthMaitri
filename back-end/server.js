223afaaffc5e2c049fde23239090eba623161f9e
Chaitanya <139270341+Chaitanya-45@users.noreply.github.com>
2024-06-09 16:04:57 +0530

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,34 +1,25 @@
-// Import necessary modules
 const express = require('express');
 const firebaseAdmin = require('firebase-admin');
 const cors = require('cors'); // Import the CORS middleware
 
-// Initialize Express app
 const app = express();
 
-// Initialize Firebase Admin SDK
 const serviceAccount = require('./svasthmaitri-firebase-adminsdk.json');
 firebaseAdmin.initializeApp({
   credential: firebaseAdmin.credential.cert(serviceAccount),
   databaseURL: 'https://svasthmaitri-default-rtdb.firebaseio.com/'
 });
 
-// Middleware to parse JSON bodies
 app.use(express.json());
 
-// Use CORS middleware to enable cross-origin requests
 app.use(cors());
 
-// Define route to handle medicine form submissions
 app.post('/submit-medicine', (req, res) => {
-  // Extract form data from request body
   const { medicineName, quantity, location, email, expiryDate } = req.body;
 
-  // Create a reference to the Firebase Realtime Database
   const db = firebaseAdmin.database();
   const medicineDonationsRef = db.ref('medicineDonations');
 
-  // Push form data to the database
   medicineDonationsRef.push({
     medicineName,
     quantity,
@@ -45,16 +36,14 @@ app.post('/submit-medicine', (req, res) => {
     });
 });
 
-// Define route to handle blood donation form submissions
 app.post('/submit-blood', (req, res) => {
-  // Extract form data from request body
   const { fullName, age, bloodType, location, email } = req.body;
 
-  // Create a reference to the Firebase Realtime Database
+  
   const db = firebaseAdmin.database();
   const bloodDonationsRef = db.ref('bloodDonations');
 
-  // Push form data to the database
+  
   bloodDonationsRef.push({
     fullName,
     age,
@@ -71,16 +60,16 @@ app.post('/submit-blood', (req, res) => {
     });
 });
 
-// Define route to handle medical equipment form submissions
+
 app.post('/submit-equipment', (req, res) => {
   // Extract form data from request body
   const { equipmentName, quantity, condition, location, email } = req.body;
 
-  // Create a reference to the Firebase Realtime Database
+  
   const db = firebaseAdmin.database();
   const equipmentDonationsRef = db.ref('equipmentDonations');
 
-  // Push form data to the database
+  
   equipmentDonationsRef.push({
     equipmentName,
     quantity,
@@ -97,13 +86,12 @@ app.post('/submit-equipment', (req, res) => {
     });
 });
 
-// Error handling middleware
+
 app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
